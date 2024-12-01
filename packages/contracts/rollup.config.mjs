75cdc9c3b77ab235242c0df09dfc0ea877ce7edd
Hebilicious <xsh4k3@gmail.com>
2024-12-01 23:46:22 +0700

    chore: fix sourcemap

--- packages/contracts/rollup.config.mjs
+++ packages/contracts/rollup.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig([
   {
     input: "src/index.ts",
     output: [{ sourcemap: true, dir: "dist", format: "es" }],
-    plugins: [typescript({ tsconfig: "tsconfig.build.esm.json", sourceMap: true })]
+    plugins: [typescript({ tsconfig: "tsconfig.build.esm.json" })]
   },
   {
     input: "dist/index.d.ts",
@@ -24,15 +24,17 @@ export default defineConfig([
 
         const filesToKeep = ["index.js", "index.js.map", "index.d.ts"]
 
+        // const removed = []
         for (const file of files) {
           const fullPath = path.join(distPath, file)
           const shouldKeep = filesToKeep.some(keep => file.endsWith(keep))
           if (!shouldKeep) {
             await fs.rm(fullPath, { recursive: true, force: true })
+            // removed.push(fullPath)
             // console.log(`Removing: ${file}`)
           }
         }
-        // console.log("Finished!!", __dirname)
+        // console.log("Removed Files", removed)
       }
     }]
   }
