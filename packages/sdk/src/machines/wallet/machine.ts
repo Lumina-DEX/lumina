d69b3dd2feac6096f7d6c85578ac469f2c081df3
Hebilicious <xsh4k3@gmail.com>
2024-12-26 03:34:30 +0100

    fix(sdk): fetching balance

--- packages/sdk/src/machines/wallet/machine.ts
+++ packages/sdk/src/machines/wallet/machine.ts
@@ -99,9 +99,10 @@ export const createWalletMachine = (
 				const publicKey = input.address
 				const name = input.token?.symbol.toLocaleUpperCase() ?? "MINA"
 				const decimal = input.token?.decimal ?? 1e9
-				const settings = input.token
-					? { tokenId: TokenId.derive(PublicKey.fromBase58(input.token.address)), publicKey }
-					: { publicKey }
+				const settings = {
+					tokenId: input.token ? TokenId.derive(PublicKey.fromBase58(input.token.address)) : null,
+					publicKey
+				}
 
 				const queries = Object.fromEntries(
 					input.networks.map((network) => [
