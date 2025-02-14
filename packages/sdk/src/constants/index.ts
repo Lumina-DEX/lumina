c469c72c9cf4a4c259a57cbbf05a8fb36c1a5ac8
Hebilicious <xsh4k3@gmail.com>
2025-02-15 05:00:48 +0700

    feat: updated chain information

--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -1,49 +1,63 @@
-type NetworkLayer = "mina" | "zeko"
+export type NetworkLayer = "mina" | "zeko"
 
-export type ChainNetwork = "mainnet" | "berkeley" | "testnet"
+export type ChainNetwork = "mainnet" | "devnet" | "testnet"
 
-export type NetworkUri = Exclude<`${NetworkLayer}:${ChainNetwork}`, "zeko:berkeley">
+export type NetworkUri = Exclude<
+	`${NetworkLayer}:${ChainNetwork}`,
+	"zeko:devnet" | "mina:testnet"
+>
 
 export const networks = [
 	"mina:mainnet",
-	"mina:berkeley",
-	"mina:testnet",
+	"mina:devnet",
 	"zeko:testnet",
 	"zeko:mainnet"
 ] as const
 
 export const urls = {
 	"mina:mainnet": "https://api.minascan.io/node/mainnet/v1/graphql",
-	"mina:berkeley": "https://api.minascan.io/node/berkeley/v1/graphql",
-	"mina:testnet": "https://api.minascan.io/node/devnet/v1/graphql",
+	"mina:devnet": "https://api.minascan.io/node/devnet/v1/graphql",
 	"zeko:testnet": "https://devnet.zeko.io/graphql",
-	"zeko:mainnet": "https://devnet.zeko.io/graphql"
+	"zeko:mainnet": "NOT_IMPLEMENTED"
 } as const
 
 export const archiveUrls = {
 	"mina:mainnet": "https://api.minascan.io/archive/mainnet/v1/graphql",
-	"mina:berkeley": "https://api.minascan.io/archive/devnet/v1/graphql",
-	"mina:testnet": "https://api.minascan.io/archive/devnet/v1/graphql",
+	"mina:devnet": "https://api.minascan.io/archive/devnet/v1/graphql",
 	"zeko:testnet": "https://devnet.zeko.io/graphql",
-	"zeko:mainnet": "https://devnet.zeko.io/graphql"
+	"zeko:mainnet": "NOT_IMPLEMENTED"
 } as const
 
 // TODO: Add missing factories
 export const luminadexFactories = {
-	"mina:testnet": "B62qo8GFnNj3JeYq6iUUXeHq5bqJqPQmT5C2cTU7YoVc4mgiC8XEjHd"
+	"mina:devnet": "B62qo8GFnNj3JeYq6iUUXeHq5bqJqPQmT5C2cTU7YoVc4mgiC8XEjHd",
+	"mina:mainnet": "NOT_IMPLEMENTED",
+	"zeko:testnet": "NOT_IMPLEMENTED",
+	"zeko:mainnet": "NOT_IMPLEMENTED"
 } as const
 
 // TODO: Add missing faucets
 export const chainFaucets = {
-	"mina:testnet": {
+	"mina:devnet": {
 		address: "B62qkUoCRMDTndXpGan1g7iVPAGnXASVT3fqV8QnGqJ5KNiRhnS8nyq",
 		tokenAddress: "B62qn71xMXqLmAT83rXW3t7jmnEvezaCYbcnb9NWYz85GTs41VYGDha",
 		tokenId: "wZmPhCrDVraeYcB3By5USJCJ9KCMLYYp497Zuby2b8Rq3wTcbn"
 	},
-	"mina:mainnet": { address: "NOT Implemented", tokenAddress: "", tokenId: "" },
-	"mina:berkeley": { address: "testnet-address-2", tokenAddress: "", tokenId: "" },
-	"zeko:testnet": { address: "zeko-test", tokenAddress: "", tokenId: "" },
-	"zeko:mainnet": { address: "zeko-test", tokenAddress: "", tokenId: "" }
+	"mina:mainnet": {
+		address: "NOT_IMPLEMENTED",
+		tokenAddress: "NOT_IMPLEMENTED",
+		tokenId: "NOT_IMPLEMENTED"
+	},
+	"zeko:testnet": {
+		address: "NOT_IMPLEMENTED",
+		tokenAddress: "NOT_IMPLEMENTED",
+		tokenId: "NOT_IMPLEMENTED"
+	},
+	"zeko:mainnet": {
+		address: "NOT_IMPLEMENTED",
+		tokenAddress: "NOT_IMPLEMENTED",
+		tokenId: "NOT_IMPLEMENTED"
+	}
 } as const
 
 export const MINA_ADDRESS = "MINA"
@@ -51,12 +65,11 @@ export const MINA_ADDRESS = "MINA"
 export const luminaCdnOrigin = "https://luminadex-contracts-cdn.hebilicious.workers.dev"
 
 export const poolInstance = {
-	"mina:testnet": {
+	"mina:devnet": {
 		signer: "EKE9dyeMmvz6deCC2jD9rBk7d8bG6ZDqVno8wRe8tAbQDussfBYi",
 		user0: "B62qrUAGW6S4pSBcZko2LdbUAhtLd15zVs9KtQedScBvwuZVbcnej35"
 	},
-	"mina:mainnet": { signer: "", user0: "" },
-	"mina:berkeley": { signer: "", user0: "" },
-	"zeko:testnet": { signer: "", user0: "" },
-	"zeko:mainnet": { signer: "", user0: "" }
+	"mina:mainnet": { signer: "NOT_IMPLEMENTED", user0: "NOT_IMPLEMENTED" },
+	"zeko:mainnet": { signer: "NOT_IMPLEMENTED", user0: "NOT_IMPLEMENTED" },
+	"zeko:testnet": { signer: "NOT_IMPLEMENTED", user0: "NOT_IMPLEMENTED" }
 } as const
