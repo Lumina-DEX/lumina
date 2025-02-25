307f92194273ca85ce4dbc06b01f92f943d9c819
eddy <be@be-technology.fr>
2025-02-25 15:04:12 +0100

    add transfer

--- packages/contracts/src/pool/Order.ts
+++ packages/contracts/src/pool/Order.ts
@@ -1,3 +1,4 @@
+import { FungibleToken } from "mina-fungible-token"
 import {
   Field,
   MerkleMap,
@@ -14,24 +15,40 @@ import {
 
 export class AddOrderEvent extends Struct({
   sender: PublicKey,
-  token: PublicKey,
-  amount: UInt64
+  tokenIn: PublicKey,
+  amountIn: UInt64,
+  tokenOut: PublicKey,
+  amountOut: UInt64
 }) {
   constructor(value: {
     sender: PublicKey
-    token: PublicKey
-    amount: UInt64
+    tokenIn: PublicKey
+    amountIn: UInt64
+    tokenOut: PublicKey
+    amountOut: UInt64
   }) {
     super(value)
   }
 
   hash(): Field {
-    return Poseidon.hash(this.sender.toFields().concat(this.token.toFields().concat(this.amount.toFields())))
+    return Poseidon.hash(
+      this.sender.toFields()
+        .concat(
+          this.tokenIn.toFields()
+            .concat(
+              this.amountIn.toFields()
+                .concat(
+                  this.tokenOut.toFields()
+                    .concat(this.amountOut.toFields())
+                )
+            )
+        )
+    )
   }
 }
 
 /**
- * Test code don't use it
+ * Test code don't use it in production
  */
 export class Order extends SmartContract {
   @state(Field)
@@ -52,7 +69,13 @@ export class Order extends SmartContract {
   }
 
   @method
-  async addOrder(token: PublicKey, amount: UInt64, witness: MerkleMapWitness) {
+  async addOrder(
+    tokenIn: PublicKey,
+    amountIn: UInt64,
+    tokenOut: PublicKey,
+    amountOut: UInt64,
+    witness: MerkleMapWitness
+  ) {
     const indexOrder = this.indexOrder.getAndRequireEquals()
     const merkleOrder = this.merkleOrder.getAndRequireEquals()
 
@@ -64,7 +87,12 @@ export class Order extends SmartContract {
     witnessKey.assertEquals(newKey, "Witness incorrect")
 
     const sender = this.sender.getAndRequireSignature()
-    const orderEvent = new AddOrderEvent({ sender, token, amount })
+
+    // send token to the order book
+    const tokenContract = new FungibleToken(tokenIn)
+    tokenContract.transfer(sender, this.address, amountIn)
+
+    const orderEvent = new AddOrderEvent({ sender, tokenIn, amountIn, tokenOut, amountOut })
 
     const [witnessRootAfter] = witness.computeRootAndKey(orderEvent.hash())
 
@@ -72,5 +100,28 @@ export class Order extends SmartContract {
     this.indexOrder.set(newKey)
 
     this.emitEvent("addOrder", orderEvent)
+  }
+
+  @method
+  async fillOrder(orderA: Field, orderB: Field, witness: MerkleMapWitness) {
+    // const indexOrder = this.indexOrder.getAndRequireEquals()
+    // const merkleOrder = this.merkleOrder.getAndRequireEquals()
+
+    // // we start from the next key who are supposed to be empty
+    // const empty = Field.empty()
+    // const newKey = indexOrder.add(1)
+    // const [witnessRootBefore, witnessKey] = witness.computeRootAndKey(empty)
+    // witnessRootBefore.assertEquals(merkleOrder, "Witness incorrect")
+    // witnessKey.assertEquals(newKey, "Witness incorrect")
+
+    // const sender = this.sender.getAndRequireSignature()
+    // const orderEvent = new AddOrderEvent({ sender, tokenIn, amountIn, tokenOut, amountOut })
+
+    // const [witnessRootAfter] = witness.computeRootAndKey(orderEvent.hash())
+
+    // this.merkleOrder.set(witnessRootAfter)
+    // this.indexOrder.set(newKey)
+
+    // this.emitEvent("addOrder", orderEvent)
   }
 }
