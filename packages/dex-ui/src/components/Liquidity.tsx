ce5aa1fdaaddf7d1df72228bb93cad180e243ed6
eddy <eddyboughioul@hotmail.fr>
2025-03-27 08:52:48 +0100

    correct liquidity field

--- packages/dex-ui/src/components/Liquidity.tsx
+++ packages/dex-ui/src/components/Liquidity.tsx
@@ -67,7 +67,7 @@ const Liquidity = ({}) => {
 
 				const from = result.amountAIn / 10 ** 9
 				const to = result.amountBIn / 10 ** 9
-				const liquidity = result.supplyMin / 10 ** 9
+				const liquidity = result.liquidity / 10 ** 9
 				setFromAmount(from.toFixed(2).toString())
 				setToAmount(to.toFixed(2).toString())
 				setLiquidityMinted(liquidity)
