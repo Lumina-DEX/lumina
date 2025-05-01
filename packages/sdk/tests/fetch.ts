3071bfd303ef7ca72e28b8db974729f865a175da
Hebilicious <xsh4k3@gmail.com>
2025-05-01 19:02:59 +0700

    chore: add testing script

---
+++ packages/sdk/tests/fetch.ts
@@ -0,0 +1,22 @@
+import {
+	fetchAllPoolsFromPoolFactory,
+	fetchAllTokensFromPoolFactory
+} from "../src/helpers/blockchain"
+
+console.time("fetchAllPoolsFromPoolFactory - mina:devnet")
+const pools = await fetchAllPoolsFromPoolFactory({ network: "mina:devnet" })
+console.timeEnd("fetchAllPoolsFromPoolFactory - mina:devnet")
+
+console.time("fetchAllPoolsFromPoolFactory - zeko:testnet")
+const poolsZeko = await fetchAllPoolsFromPoolFactory({ network: "zeko:testnet" })
+console.timeEnd("fetchAllPoolsFromPoolFactory - zeko:testnet")
+
+console.time("fetchAllTokensFromPoolFactory - mina:devnet")
+const tokens = await fetchAllTokensFromPoolFactory({ network: "mina:devnet" })
+console.timeEnd("fetchAllTokensFromPoolFactory - mina:devnet")
+
+console.time("fetchAllTokensFromPoolFactory - zeko:testnet")
+const tokensZeko = await fetchAllTokensFromPoolFactory({ network: "zeko:testnet" })
+console.timeEnd("fetchAllTokensFromPoolFactory - zeko:testnet")
+
+console.log({ pools, tokens, poolsZeko, tokensZeko })
