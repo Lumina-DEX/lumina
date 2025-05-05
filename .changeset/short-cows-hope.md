bed7d24cee6cb819630ecdb279537a1a3c908414
Heb <xsh4k3@gmail.com>
2025-05-05 22:07:05 +0700

    fix: dataclone error while loading contracts (#74)

---
+++ .changeset/short-cows-hope.md
@@ -0,0 +1,5 @@
+---
+"@lumina-dex/sdk": patch
+---
+
+fix dataclone errors while loading contracts
--- packages/sdk/src/dex/luminadex-worker.ts
+++ packages/sdk/src/dex/luminadex-worker.ts
@@ -122,12 +122,8 @@ const loadContracts = async () => {
 	logger.success("Loaded contracts")
 }
 
-export interface CompileContract {
-	contract: ContractName
-}
-
 let cache: ReturnType<typeof readCache>
-const compileContract = async ({ contract }: CompileContract) => {
+const compileContract = async ({ contract }: { contract: ContractName }) => {
 	if (!cache) {
 		const cacheFiles = await fetchZippedContracts()
 		cache = readCache(cacheFiles)
