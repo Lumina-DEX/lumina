eca3df809ee3d46f62b5a6bf21ca07563802dc7f
eddy <eddyboughioul@hotmail.fr>
2025-05-23 10:23:28 +0200

    add create pool to cloudworker

--- packages/cloudworker/src/PoolWorker.ts
+++ packages/cloudworker/src/PoolWorker.ts
@@ -19,16 +19,22 @@ import {
 	AccountUpdate,
 	Cache,
 	UInt64,
-	fetchAccount
+	fetchAccount,
+	Poseidon,
+	Signature,
+	Bool,
+	MerkleMap
 } from "o1js"
 import { Pool } from "./pool/Pool"
 import { PoolFactory } from "./pool/PoolFactory"
 import { PoolTokenHolder } from "./pool/PoolTokenHolder"
+import { SignatureRight } from "./pool/Multisig"
 
 export class PoolWorker extends zkCloudWorker {
 	static fungibleTokenVerificationKey: VerificationKey | undefined = undefined
 	static poolVerificationKey: VerificationKey | undefined = undefined
 	static poolTokenVerificationKey: VerificationKey | undefined = undefined
+	static poolFactoryVerificationKey: VerificationKey | undefined = undefined
 	readonly cache: Cache
 
 	constructor(cloud: Cloud) {
@@ -83,13 +89,53 @@ export class PoolWorker extends zkCloudWorker {
 		}
 	}
 
+	private async compileFactory(compileSmartContracts: boolean = true): Promise<void> {
+		try {
+			console.time("compiled factory")
+
+			if (compileSmartContracts === false) {
+				console.timeEnd("compiled factory")
+				return
+			}
+
+			if (PoolWorker.fungibleTokenVerificationKey === undefined) {
+				console.time("compiled fungibleToken")
+				PoolWorker.fungibleTokenVerificationKey = (
+					await FungibleToken.compile({
+						cache: this.cache
+					})
+				).verificationKey
+				console.timeEnd("compiled fungibleToken")
+			}
+
+			if (PoolWorker.poolFactoryVerificationKey === undefined) {
+				console.time("compiled pool factory")
+				PoolWorker.poolFactoryVerificationKey = (
+					await PoolFactory.compile({
+						cache: this.cache
+					})
+				).verificationKey
+				console.timeEnd("compiled pool factory")
+			}
+
+			console.timeEnd("compiled factory")
+		} catch (error) {
+			console.error("Error in compile factory, restarting container", error)
+			// Restarting the container, see https://github.com/o1-labs/o1js/issues/1651
+			await this.cloud.forceWorkerRestart()
+			throw error
+		}
+	}
+
 	public async execute(transactions: string[]): Promise<string | undefined> {
 		if (this.cloud.args === undefined) throw new Error("this.cloud.args is undefined")
 		const args = JSON.parse(this.cloud.args)
 
 		switch (this.cloud.task) {
 			case "swap":
-				return await this.swap({ ...args, isMany: false })
+				return await this.swap({ ...args })
+			case "createPool":
+				return await this.createPool({ ...args })
 
 			default:
 				throw new Error(`Unknown task: ${this.cloud.task}`)
@@ -123,6 +169,7 @@ export class PoolWorker extends zkCloudWorker {
 		balanceOutMin: number
 		balanceInMax: number
 		factory: string
+		tx: string
 	}): Promise<string> {
 		try {
 			const privateKey = PrivateKey.random()
@@ -185,6 +232,120 @@ export class PoolWorker extends zkCloudWorker {
 						args.from === MINA_ADDRESS ? "swapFromMinaToToken" : "swapFromTokenToToken"
 					](...swapArgList)
 					await zkToken.approveAccountUpdate(zkPoolHolder.self)
+				}
+			})
+
+			if (tx === undefined) throw new Error("tx is undefined")
+			const txProved = await tx.prove()
+			const txJSON = txProved.toJSON()
+
+			console.timeEnd("prepared tx")
+
+			return this.stringifyJobResult({
+				success: true,
+				tx: txJSON
+			})
+		} catch (error) {
+			return this.stringifyJobResult({
+				success: false,
+				tx: "",
+				error: String(error)
+			})
+		}
+	}
+
+	private async createPool(args: {
+		tokenA: string
+		tokenB: string
+		user: string
+		factory: string
+		user0: string
+		signer: string
+		tx: string
+	}): Promise<string> {
+		try {
+			const poolKey = PrivateKey.random()
+			const MINA_ADDRESS = "MINA"
+
+			console.log("Pool Key", poolKey.toBase58())
+
+			await this.compile()
+
+			console.log(`Sending tx...`)
+			console.time("prepared tx")
+
+			const allRight = new SignatureRight(
+				Bool(true),
+				Bool(true),
+				Bool(true),
+				Bool(true),
+				Bool(true),
+				Bool(true)
+			)
+			const deployRight = SignatureRight.canDeployPool()
+
+			const merkle = new MerkleMap()
+			// TODO: temporary solution for testnet
+			const signerPk = PrivateKey.fromBase58(args.signer)
+			const user0Pk = PublicKey.fromBase58(args.user0)
+			const user1 = signerPk.toPublicKey()
+
+			const ownerPublic = PublicKey.fromBase58(
+				"B62qjabhmpW9yfLbvUz87BR1u462RRqFfXgoapz8X3Fw8uaXJqGG8WH"
+			)
+			const signer1Public = PublicKey.fromBase58(
+				"B62qrgWEGhgXQ5PnpEaeJqs1MRx4Jiw2aqSTfyxAsEVDJzqNFm9PEQt"
+			)
+			const signer2Public = PublicKey.fromBase58(
+				"B62qkfpRcsJjByghq8FNkzBh3wmzLYFWJP2qP9x8gJ48ekfd6MVXngy"
+			)
+			const signer3Public = PublicKey.fromBase58(
+				"B62qic5sGvm6QvFzJ92588YgkKxzqi2kFeYydnkM8VDAvY9arDgY6m6"
+			)
+			const approvedSignerPublic = PublicKey.fromBase58(
+				"B62qpko6oWqKU4LwAaT7PSX3b6TYvroj6umbpyEXL5EEeBbiJTUMU5Z"
+			)
+
+			merkle.set(Poseidon.hash(ownerPublic.toFields()), allRight.hash())
+			merkle.set(Poseidon.hash(signer1Public.toFields()), allRight.hash())
+			merkle.set(Poseidon.hash(signer2Public.toFields()), allRight.hash())
+			merkle.set(Poseidon.hash(signer3Public.toFields()), allRight.hash())
+			merkle.set(Poseidon.hash(approvedSignerPublic.toFields()), deployRight.hash())
+			const signature = Signature.create(signerPk, poolKey.toPublicKey().toFields())
+
+			const witness = merkle.getWitness(Poseidon.hash(approvedSignerPublic.toFields()))
+
+			const factoryKey = PublicKey.fromBase58(args.factory)
+
+			const zkFactory = new PoolFactory(factoryKey)
+
+			await fetchAccount({ publicKey: factoryKey })
+
+			const isMinaTokenPool = args.tokenA === MINA_ADDRESS || args.tokenB === MINA_ADDRESS
+
+			const tx = await Mina.transaction(PublicKey.fromBase58(args.user), async () => {
+				AccountUpdate.fundNewAccount(PublicKey.fromBase58(args.user), 4)
+				if (isMinaTokenPool) {
+					const token = args.tokenA === MINA_ADDRESS ? args.tokenB : args.tokenA
+					await zkFactory.createPool(
+						poolKey.toPublicKey(),
+						PublicKey.fromBase58(token),
+						user1,
+						signature,
+						witness,
+						deployRight
+					)
+				}
+				if (!isMinaTokenPool) {
+					await zkFactory.createPoolToken(
+						poolKey.toPublicKey(),
+						PublicKey.fromBase58(args.tokenA),
+						PublicKey.fromBase58(args.tokenB),
+						user1,
+						signature,
+						witness,
+						deployRight
+					)
 				}
 			})
 
