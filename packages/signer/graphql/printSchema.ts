0f96bfe6fa7903163ee62279ac32809ad295ab71
Youtpout <EddyBoughioul@hotmail.fr>
2025-10-17 16:00:47 +0200

    rollback bun script, use tsdown

--- packages/signer/graphql/printSchema.ts
+++ packages/signer/graphql/printSchema.ts
@@ -1,15 +1,7 @@
-import { writeFile } from "node:fs/promises"
-import { dirname, join } from "node:path"
-import { fileURLToPath } from "node:url"
 import { lexicographicSortSchema, printSchema } from "graphql"
+
 import { schema } from "../src/graphql"
 
-// Resolve the current directory (Node equivalent of import.meta.dir)
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-// Generate the schema text
 const schemaToPrint = printSchema(lexicographicSortSchema(schema))
 
-// Write to ./schema.graphql
-await writeFile(join(__dirname, "schema.graphql"), schemaToPrint)
+await Bun.write(`${import.meta.dir}/schema.graphql`, schemaToPrint)
