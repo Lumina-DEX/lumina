a80ed49ec0ac9a3738d0e5392c1b899658c48759
Youtpout <EddyBoughioul@hotmail.fr>
2025-12-20 11:33:31 +0100

    correct tsdown cdn

--- packages/contracts-cdn/tsdown.config.ts
+++ packages/contracts-cdn/tsdown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsdown"
 
 export default defineConfig({
 	entry: ["./container/index.ts"],
+	format: "esm",
 	noExternal: ["graphql-request"],
 	outDir: "output",
 	platform: "browser"
